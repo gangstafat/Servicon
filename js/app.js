c5139a088552874cfab1d3c570477f1f5c087dc2
Huy Nguyen <huynguyen.me@gmail.com>
2015-04-13 00:32:34 -0700

    Update Fixes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+
+/*$(document).ready(function() {
+	$(".availability .tab-title a").hover(function() {
+		var href = $(this).attr('href');
+       	$(".availability .tabs-content "+"href").addClass('active');
+	});
+});*/
